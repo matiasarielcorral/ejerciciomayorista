938a40bcaff3388b8cc847f918a7772943dd35d4
matias notebook <matiasariel.corral@gmail.com>
2025-06-05 20:17:38 -0400

    codigo actualizado

--- src/Main.java
+++ src/Main.java
@@ -13,18 +13,30 @@ public class Main {
         double precioPaquetes = 100.0;
         double valoraPagar, costoEnvio;
         double descuentoPaquete = 0.05;
-
         Scanner ingresapaquetes = new Scanner(System.in);
 
-        System.out.println("Ingrese cantidad de paquetes a comprar: ");
+
+        System.out.println("Bievenido a la compra de paquetes al por mayor" + System.lineSeparator() +
+                "Cada Paquete tiene un costo de: " + precioPaquetes + " Usd" + System.lineSeparator()+
+                "Ingrese cantidad de paquetes a comprar: ");
         cantidadPaquetes = ingresapaquetes.nextInt();
 
         if (cantidadPaquetes < 5)
         {
-            System.out.println("No se realizan ventas por menos de 5 paquetes");
-        } else
+
+            if (cantidadPaquetes <0)
+            {
+                System.out.println("No se pueden ingresar valores negativos");
+            }
+            else
+            {
+                System.out.println("No se realizan ventas por menos de 5 paquetes");
+            }
+
+        }
+        else
           {
-            if (cantidadPaquetes >= 5 && cantidadPaquetes <= 15)
+            if (cantidadPaquetes <= 15)
             {
                 System.out.println("El envio tiene un costo de 10 usd");
                 costoEnvio = 10;
